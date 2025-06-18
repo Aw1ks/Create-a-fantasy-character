e0b7f254a57205d35f93732137c1ebebfeeb783e
Aw1ks <aw1ksss331@gmail.com>
2025-06-18 20:08:19 +0300

    added the ability to select one race from the list

--- template.py
+++ template.py
@@ -7,10 +7,20 @@ env = Environment(
 template = env.get_template('template.html')
 
 # пишем код для получения данных карточки
+list_races = [
+    'Люминары',
+    'Кристаллы',
+    'Аэроиды',
+    'Терраги'
+]
+
 user_name = input('Введите имя пользователя: ')
+user_race = input('Выберите расу из предложенных:\n' + '\n'.join(list_races) + '\n')
+
 
 rendered_page = template.render(
     name=user_name.capitalize(),
+    race=user_race,
     # пишем код, что добавляется к карточке
 )
 
