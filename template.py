afa98afa57bf93131962c825cba1c523e53bb9ea
Aw1ks <aw1ksss331@gmail.com>
2025-06-18 20:26:47 +0300

    added default outstanding characteristic and class image

--- template.py
+++ template.py
@@ -24,22 +24,67 @@ list_сlasses = [
     'Бард'
 ]
 
+clases_base = {
+    'Маг': {
+        'intelligence': 15,
+        'strength': random.randint(1, 3),
+        'agility': random.randint(1, 3),
+        'luck': random.randint(1, 3),
+        'temper': random.randint(1, 3),
+        'image_path': 'images/wizard.png',
+    },
+    'Воин': {
+        'strength': 15,
+        'intelligence': random.randint(1, 3),
+        'agility': random.randint(1, 3),
+        'luck': random.randint(1, 3),
+        'temper': random.randint(1, 3),
+        'image_path': 'images/warrior.png',
+    },
+    'Охотник': {
+        'agility': 15,
+        'strength': random.randint(1, 3),
+        'intelligence': random.randint(1, 3),
+        'luck': random.randint(1, 3),
+        'temper': random.randint(1, 3),
+        'image_path': 'images/archer.png',
+    },
+    'Ассасин': {
+        'luck': 15,
+        'strength': random.randint(1, 3),
+        'agility': random.randint(1, 3),
+        'intelligence': random.randint(1, 3),
+        'temper': random.randint(1, 3),
+        'image_path': 'images/assasin.png',
+    },
+    'Бард': {
+        'temper': 15,
+        'strength': random.randint(1, 3),
+        'agility': random.randint(1, 3),
+        'intelligence': random.randint(1, 3),
+        'luck': random.randint(1, 3),
+        'image_path': 'images/bard.webp',
+    },
+}
+
 user_name = input('\nВведите имя пользователя: ')
 user_race = input('\nВыберите расу из предложенных:\n' + '\n'.join(list_races) + '\n')
 user_class = input('\nВыберите класс из предложенных:\n' + '\n'.join(list_сlasses) + '\n')
 
-
+characteristics_selected_class = clases_base[user_class]
 
 rendered_page = template.render(
     name=user_name.capitalize(),
     race=user_race.capitalize(),
     character_class=user_class.capitalize(),
 
-    strength=random.randint(1, 3),
-    agility=random.randint(1, 3),
-    intelligence=random.randint(1, 3),
-    luck=random.randint(1, 3),
-    temper=random.randint(1, 3),
+    strength=characteristics_selected_class['strength'],
+    agility=characteristics_selected_class['agility'],
+    intelligence=characteristics_selected_class['intelligence'],
+    luck=characteristics_selected_class['luck'],
+    temper=characteristics_selected_class['temper'],
+
+    image=characteristics_selected_class['image_path'],
     # пишем код, что добавляется к карточке
 )
 
